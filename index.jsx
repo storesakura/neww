d38190c0164f462b713e1048e14a3d6fe28bac27
storesakura <98824222+storesakura@users.noreply.github.com>
2022-02-04 11:15:21 +0300

    Create index.jsx

---
+++ index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+
+window._root = document.getElementById('root')
+ReactDOM.render(<App />, window._root)
+
+
+
+// WEBPACK FOOTER //
+// ./src/index.js
